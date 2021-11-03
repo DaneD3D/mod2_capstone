05303af64be066d4bece142a8f30977e0bc8296a
Dane Danovskis <DaneD319@gmail.com>
2021-11-03 10:12:48 -0400

    Uploading Danes morning progress

--- 18_Capstone/student/java/tenmo-server/src/main/java/com/techelevator/tenmo/model/User.java
+++ 18_Capstone/student/java/tenmo-server/src/main/java/com/techelevator/tenmo/model/User.java
@@ -1,5 +1,6 @@
 package com.techelevator.tenmo.model;
 
+import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -11,6 +12,7 @@ public class User {
    private String password;
    private boolean activated;
    private Set<Authority> authorities = new HashSet<>();
+   private BigDecimal balance;
 
    public User() { }
 
@@ -59,6 +61,14 @@ public class User {
 
    public void setAuthorities(Set<Authority> authorities) {
       this.authorities = authorities;
+   }
+
+   public BigDecimal getBalance() {
+      return balance;
+   }
+
+   public void setBalance(BigDecimal balance) {
+      this.balance = balance;
    }
 
    public void setAuthorities(String authorities) {
